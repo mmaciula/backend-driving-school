9b6cdeed8a33e8010abb6c3dab78708989e0b7b1
mariusz <mariusz.maciula@gmail.com>
2020-11-10 09:36:04 +0100

    Added missing sign in integration test & change passwords inside data.sql to their proper form (encrypted)

--- src/test/java/pl/superjazda/drivingschool/authentication/AuthControllerTest.java
+++ src/test/java/pl/superjazda/drivingschool/authentication/AuthControllerTest.java
@@ -9,10 +9,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import pl.superjazda.drivingschool.jwt.Login;
 import pl.superjazda.drivingschool.jwt.Register;
+import pl.superjazda.drivingschool.user.User;
+import pl.superjazda.drivingschool.user.UserRepository;
+
+import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -26,12 +32,24 @@ public class AuthControllerTest {
 
     @Test
     public void shouldRegisterNewUserTest() throws Exception {
-        Register register = new Register("AuthTest", "auth@domain.com", "test1234", "Joe", "Doe");
+        Register register = new Register("AuthTest", "auth@domain.com", "AuthTest123", "Joe", "Doe");
 
         mockMvc.perform(post("/api/auth/signup")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(register)))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("User registered successfully!"));
+    }
+
+    @Test
+    public void shouldLogInUserTest() throws Exception {
+        Login credentials = new Login("student", "student123");
+
+        mockMvc.perform(post("/api/auth/signin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(credentials)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("student"));
     }
 }
