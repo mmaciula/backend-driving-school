1ad2474c02f3caafa6647dce1c1b790757906fdc
mariusz <mariusz.maciula@gmail.com>
2021-06-11 22:55:49 +0200

    Add profiles & changed passwords to encrypted using jasypt

--- src/main/java/pl/superjazda/drivingschool/Application.java
+++ src/main/java/pl/superjazda/drivingschool/Application.java
@@ -1,9 +1,11 @@
 package pl.superjazda.drivingschool;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class Application {
 
 	public static void main(String[] args) {
